b5deb0c3d56ffaf5167c13c66e9cf65efac7f6d6
Roberto Horst <roberto@naip.com.br>
2021-02-09 22:28:40 -0300

    Aula 01

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
+import { loadGetInitialProps } from 'next/dist/next-server/lib/utils'
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+function Title(props){
+  return <h1>{props.children}</h1>
+}
 
 export default function Home() {
-  return <div></div>
+  return <div><Title>Teste de titulo</Title> outra linha ...</div>
 }
