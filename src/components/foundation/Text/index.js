0aebfc56c86bf49f67ab4a609e8821fb28eca3e9
Roberto Horst <roberto@naip.com.br>
2021-02-15 17:32:31 -0300

    Aula 05

---
+++ src/components/foundation/Text/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import get from 'lodash/get';
+import styled from 'styled-components';
+
+const paragraph1 = `
+  ${({ theme }) => css`
+    font-size: ${theme.typographyVariants.paragraph1.fontSize};
+    font-weight: ${theme.typographyVariants.paragraph1.fontWeight};
+    line-height: ${theme.typographyVariants.paragraph1.lineHeight};
+  `}
+`;
+
+const smallestException = `
+  ${({ theme }) => css`
+    font-size: ${theme.typographyVariants.smallestException.fontSize};
+    font-weight: ${theme.typographyVariants.smallestException.fontWeight};
+    line-height: ${theme.typographyVariants.smallestException.lineHeight};
+  `}
+`;
+
+export const TextStyleVariants = {
+  smallestException,
+  paragraph1,
+};
+
+const TextBase = styled.span`
+  ${({ variant }) => TextStyleVariants[variant]}
+  color: ${({ theme, color }) => get(theme, `colors.${color}.color`)};
+`;
+
+export default function Text ( { tag, variant, children }) {
+
+    return (
+        <TextBase
+            as = {tag}
+            variant={variant}
+        >
+            {children}
+        </TextBase>
+
+    );
+} 
+
+Text.propTypes = {
+    children: PropTypes.node.isRequired,
+    tag: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'p', 'li', 'a', 'span']),
+    variant: PropTypes.oneOf(['paragraph1', 'smallestException']),
+  };
+
+Text.defaultProps = {
+    tag: 'span',
+    variant: 'paragraph1',
+}
