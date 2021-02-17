4ab704e263703a7dfcb493bb6b940bbb0292f824
Roberto Horst <roberto@naip.com.br>
2021-02-17 15:12:10 -0300

    Aula 10

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,40 @@ import React from 'react'
 import { Button } from '../src/components/commons/Button'
 import Footer from '../src/components/commons/Footer'
 import Menu from '../src/components/commons/Menu/Index'
+import { Box } from '../src/components/foundation/layout/Box'
+import { Grid } from '../src/components/foundation/layout/Grid'
 import Text from '../src/components/foundation/Text'
 
 export default function Home() {
   return (
-    <div style={{
-      flex: '1',
-      display: 'flex',
-      flexWrap: 'wrap',
-      flexDirection: 'column',
-      justifyContent: 'space-between',
-    }}>
+    <Box
+      flex="1"
+      display="flex"
+      flexWrap="wrap"
+      flexDirection="column"
+      justifyContent="space-between"
+      backgroundImage="url(/images/bubbles.svg)"
+      backgroundRepeat="no-repeat"
+      backgroundPosition="bottom right"
+    >
       <Menu />
 
-      <div>
-        <Text
+      <Grid.Container
+        marginTop={{
+          xs: '32px',
+          md: '75px',
+        }}
+      >
+        <Grid.Row>
+          <Grid.Col
+            value={{ xs: 12, md: 5 }}
+            offset={{ xs: 0, md: 1 }}
+            display="flex"
+            alignItems="flex-start"
+            justifyContent="center"
+            flexDirection="column"
+          >
+          <Text
           variant="title"
           tag="h1"
           color="tertiary.main"
@@ -50,9 +69,22 @@ export default function Home() {
         >
           Cadastrar
         </Button>
-      </div>
+          </Grid.Col>
+          <Grid.Col
+            value={{
+              xs: 12,
+              md: 6,
+            }}
+          >
+          <img
+              style={{ display: 'block', margin: 'auto' }}
+              src="https://bootcamp-alura-01-git-modulo01.omariosouto.vercel.app/images/phones.png"
+            />
+          </Grid.Col>
+        </Grid.Row>
+      </Grid.Container>
 
       <Footer />
-    </div>
+    </Box>
     )
 }
