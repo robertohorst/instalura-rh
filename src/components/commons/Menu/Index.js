6645d3565eecbf0f5d6314984611768fe45be06f
Roberto Horst <roberto@naip.com.br>
2021-03-10 00:00:21 -0300

    Aula 01 - Link do next

--- src/components/commons/Menu/Index.js
+++ src/components/commons/Menu/Index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Logo from '../../../theme/Logo';
-import Text from '../../foundation/Text';
-import Button from '../Button';
+import { Button } from '../Button';
 import MenuWrapper from './styles/MenuWrapper';
+import Text from '../../foundation/Text';
 
 const links = [
   {
@@ -19,7 +20,7 @@ const links = [
   },
 ];
 
-export default function Menu() {
+export default function Menu({ onCadastrarClick }) {
   return (
     <MenuWrapper>
       <MenuWrapper.LeftSide>
@@ -37,13 +38,17 @@ export default function Menu() {
       </MenuWrapper.CentralSide>
 
       <MenuWrapper.RightSide>
-        <Button ghost variant="secondary.main">
+        <Button ghost variant="secondary.main" href="/app/login">
           Entrar
         </Button>
-        <Button variant="primary.main">
+        <Button variant="primary.main" onClick={onCadastrarClick}>
           Cadastrar
         </Button>
       </MenuWrapper.RightSide>
     </MenuWrapper>
   );
 }
+
+Menu.propTypes = {
+  onCadastrarClick: PropTypes.func.isRequired,
+};
