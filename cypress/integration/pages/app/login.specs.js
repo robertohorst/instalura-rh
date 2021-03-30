4355d89059307bbe1836c7830bf65e6197facf7b
Roberto Horst <roberto@naip.com.br>
2021-03-29 22:18:05 -0300

    Fim da aula 03

---
+++ cypress/integration/pages/app/login.specs.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+import LoginScreenPageObject from '../../../../src/components/screens/app/LoginScreen/LoginScreen.pageObject';
+
+describe('/pages/app/login/', () => {
+  describe('when fill and submit a form login request', () => {
+    it('go to the profile page', () => {
+      // Pré Teste
+      cy.intercept('https://instalura-api-git-master-omariosouto.vercel.app')
+        .as('userLogin');
+
+      // Cenário
+      const loginScreen = new LoginScreenPageObject(cy);
+      loginScreen
+        .fillLoginForm({ user: 'omariosouto', password: 'senhasegura' })
+        .submitLoginForm();
+
+      // Asserções
+      cy.url().should('include', '/app/profile');
+
+      cy.wait('@userLogin')
+        .then((intercept) => {
+          const { token } = intercept.response.body.data;
+          cy.getCookie('APP_TOKEN')
+            .should('exist')
+            .should('have.property', 'value', token);
+        });
+    });
+  });
+});
--- pages/app/login.js
+++ pages/app/login.js
@@ -1,15 +1,86 @@
 import React from 'react';
+import { Link } from '../../src/components/commons/Link';
+import Box from '../../src/components/foundation/layout/Box';
+import Grid from '../../src/components/foundation/layout/Grid';
+import Text from '../../src/components/foundation/Text';
+import Logo from '../../src/theme/Logo';
 import websitePageHOC from '../../src/components/wrapper/WebSitePage/hoc';
+import { WebsitePageContext } from '../../src/components/wrapper/WebSitePage';
+import LoginForm from '../../src/components/patterns/FormLogin';
 
+// Essa página e desafio, e vamos dar pronto no próximo módulo o 04
 function LoginScreen() {
-  return (
-    <div>
-      Login
+  // eslint-disable-next-line no-use-before-define
+  const websitePageContext = React.useContext(WebsitePageContext);
 
-      <a href="/">
-        Voltar para a home com refresh
-      </a>
-    </div>
+  return (
+    <Grid.Container
+      display="flex"
+      flex="1"
+      alignItems="center"
+    >
+      <Grid.Row
+        flex="1"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Grid.Col
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          offset={{ lg: 2 }}
+          value={{ xs: 12, md: 6, lg: 4 }}
+          flex={1}
+        >
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            marginTop="37px"
+            marginBottom="37px"
+          >
+            <Link
+              href="/"
+              color="secondary.main"
+            >
+              <Logo size="large" />
+            </Link>
+          </Box>
+          <LoginForm />
+          <Text
+            variant="paragraph1"
+            tag="p"
+            color="tertiary.light"
+            textAlign="center"
+          >
+            {'Não tem uma conta? '}
+            <Link
+              href="/"
+              color="secondary.main"
+              onClick={(event) => {
+                event.preventDefault();
+                websitePageContext.toggleModalCadastro();
+              }}
+            >
+              Cadastre-se
+            </Link>
+          </Text>
+        </Grid.Col>
+
+        <Grid.Col value={{ xs: 12, md: 6 }}>
+          <Box
+            display="flex"
+            justifyContent="center"
+          >
+            <img
+              align="center"
+              src="https://bootcamp-alura-01-git-modulo01.omariosouto.vercel.app/images/phones.png"
+              alt="Telefones mostrando as páginas internas do app"
+            />
+          </Box>
+        </Grid.Col>
+      </Grid.Row>
+    </Grid.Container>
   );
 }
 
@@ -20,6 +91,11 @@ export default websitePageHOC(LoginScreen, {
     },
     menuProps: {
       display: false,
+    },
+    pageBoxProps: {
+      backgroundImage: 'url(/images/bubbles.svg)',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'bottom right',
     },
   },
 });
